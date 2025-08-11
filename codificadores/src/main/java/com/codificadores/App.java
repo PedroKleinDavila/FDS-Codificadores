e0baf1cbbae76ed652c4d7b64ffba7f5d603f596
Pedro Klein Davila <p.klein@edu.pucrs.br>
2025-08-11 18:46:25 -0300

    fix: main codificador type

--- codificadores/src/main/java/com/codificadores/App.java
+++ codificadores/src/main/java/com/codificadores/App.java
@@ -1,21 +1,22 @@
 package com.codificadores;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
         Codificador cod;
 
-        cod = CodificadorFactory.createCodificador( Codificador.SIMPLES);
+        cod = CodificadorFactory.createCodificador(TipoCodificador.SIMPLES);
 
-        System.out.println("Codificador: "+cod.getNome());
-        System.out.println("Versao: "+cod.getDataCriacao());
-        System.out.println("Nivel de segurança: "+cod.getNivelSeguranca());
-        
+        System.out.println("Codificador: " + cod.getNome());
+        System.out.println("Versao: " + cod.getDataCriacao());
+        System.out.println("Nivel de segurança: " + cod.getNivelSeguranca());
+
         String texto = "Este e o string a ser codificado";
         String codificado = cod.codifica(texto);
         String decodificado = cod.decodifica(codificado);
 
-        System.out.println("Texto original: "+texto);
-        System.out.println("Texto codificado: "+codificado);
-        System.out.println("Texto decodificado: "+decodificado);
+        System.out.println("Texto original: " + texto);
+        System.out.println("Texto codificado: " + codificado);
+        System.out.println("Texto decodificado: " + decodificado);
     }
 }
